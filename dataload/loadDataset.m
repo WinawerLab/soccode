c17639c2f0f7a370d74fe209d746362c636020e6
Catherine Olsson <catherio@mit.edu>
2015-03-14 13:46:18 -0400

    Allow specifying dataset by visual area string

--- dataload/loadDataset.m
+++ dataload/loadDataset.m
@@ -1,6 +1,11 @@
-function [imNumsToUse, betamnToUse] = loadDataset(n, voxNums)
+function [imNumsToUse, betamnToUse, voxNums] = loadDataset(n, whichVoxs)
 % LOAD DATASET: Encapsulates a common motif in scripts, of loading
 % a dataset, and selecting certain voxels and images
+%
+%   n - an integer from 1 to 5, indicating which dataset to use
+%   whichVoxs - either a vector of numbers, indicating which voxel numbers
+%       to use, *or* a string indicating which visual area to load voxels
+%       from (e.g. 'V1'). If empty, load a single demo voxel.
 
     if nargin < 1
         n = 3;
@@ -10,8 +15,18 @@ function [imNumsToUse, betamnToUse] = loadDataset(n, voxNums)
     end
     
     dataset = ['dataset', num2str(n, '%02d'), '.mat'];
-    load(fullfile(rootpath, ['data/input/fmri_datasets/', dataset]),'betamn','betase');
+    load(fullfile(rootpath, ['data/input/fmri_datasets/', dataset]),'betamn','betase','roi','roilabels');
+    
+    if isinteger(whichVoxs)
+        voxNums = whichVoxs;
+    elseif ischar(whichVoxs)
+        roiNum = find(strcmp(roilabels, whichVoxs));
+        voxNums = find(roi == roiNum);
+    else
+        assert(false, 'whichVoxs must be a vector of integers for voxel indices, or a string of which visual area')
+    end
  
+    
     % Which images are in the dataset?    
     if n==1 || n==2
         imNumsDataset = 1:69;
